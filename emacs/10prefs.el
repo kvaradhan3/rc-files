950f4f2f7be268288cdaf9fa012c8812298ffa70
Kannan Varadhan <kannan@ieee.org>
2019-11-12 12:37:24 -0800

    easier to specify fonts

--- emacs/10prefs.el
+++ emacs/10prefs.el
@@ -5,22 +5,30 @@
 
 (defun win:set-font (font-name)
   "Set the font to be used for this frame, and default frame alist to value"
+  (message "Set Font to %s" font-name)
   (set-default-font font-name)
   (add-to-list 'default-frame-alist (cons 'font font-name)))
 
+(defun get-font-name (foundry fontname &optional point)
+  (if point
+      (format "-%s-%s-*-*-*-*-%d-*-*-*-*-*-*-*" foundry fontname point)
+    (format "-%s-%s-*-*-*-*-*-*-*-*-*-*-*-*" foundry fontname)
+    ))
+
 (defvar win:fonts
-  '((default . "-adobe-source code pro-*-*-*-*-*-*-*-*-*-*-*-*")
+  '((default . (get-font-name "*" "inconsolata" 16))
     (fixed   . "fixed")
     (clean   . "-Schumacher-Clean-Medium-R-Normal--12-120-75-75-C-60-ISO8859-1")
     (bold1   . "lucidasanstypewriter-bold-14")
-    (source-code   . "-adobe-source code pro-*-*-*-*-*-*-*-*-*-*-*-*")
-    (source-sans   . "-adobe-source sans pro-*-*-*-*-*-*-*-*-*-*-*-*")
-    (source-serif  . "-adobe-source serif pro-*-*-*-*-*-*-*-*-*-*-*-*")
-    (inconsolata   . "-*-inconsolata-*-*-*-*-*-*-*-*-*-*-*-*")
-    (inconsolate-large . "-*-inconsolata-*-*-*-*-16-*-*-*-*-*-*-")
-    (menlo   . "-*-menlo-*-*-*-*-*-*-*-*-*-*-*-*")
-    (menlo2  . "-apple-Menlo-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1")
-    (monaco  . "-apple-Monaco-medium-normal-normal-*-*-*-*-*-m-0-iso10646-1")
+    (source-code   . (get-font-name "adobe" "source code pro"))
+    (source-sans   . (get-font-name "adobe" "source sans pro"))
+    (source-serif  . (get-font-name "adobe" "source serif pro"))
+    (inconsolata   . (get-font-name "*" "inconsolata"))
+    (inconsolate-large . (get-font-name "*" "inconsolata" 16))
+    (menlo   . (get-font-name "*" "menlo"))
+    (menlo2  . (get-font-name "apple" "Menlo"))
+    (monaco  . (get-font-name "apple" "Monaco"))
+    (sfmono  . (get-font-name "apple" "SF Mono" 14))
     ))
 
 (defun win:set-my-font (&optional font-name)
@@ -28,7 +36,7 @@
   (interactive)
   (if (not font-name)
       (setq font-name 'default))
-  (win:set-font (cdr (assoc font-name win:fonts))))
+  (win:set-font (eval (cdr (assoc font-name win:fonts)))))
 (setq my-font nil)
 
 (if (functionp 'transient-mark-mode)
