1be84fff6b6bb5b860a140378d585e5c2ba812f9
Kannan Varadhan <kannan@ieee.org>
2020-03-24 17:43:13 -0700

    remove ciscoism that conflicts w/org mode

--- emacs/90keys.el
+++ emacs/90keys.el
@@ -35,7 +35,6 @@
 (global-set-key (kbd "C-c l")	'org-store-link)
 (global-set-key (kbd "C-c a")	'org-agenda)
 (global-set-key (kbd "C-c b")   'org-iswitchb)
-(global-set-key (kbd "C-c c")   'org-insert-cpol)
 (global-set-key (kbd "C-c r")	'remember)
 
 ;
