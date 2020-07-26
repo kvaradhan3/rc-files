35f7082a5e0c0c8e3461eb9432af792649f0cc93
Kannan Varadhan <kannan@ieee.org>
2020-07-26 10:31:43 -0700

    switch to use-package

--- emacs/30org-mode.el
+++ emacs/30org-mode.el
@@ -1,58 +1,64 @@
 ;;;
 ;;; Org Mode
 ;;;
-(add-to-list 'auto-mode-alist '("\\.\\(org\\|org_archive\\|txt\\)$" . org-mode))
-;;; (require 'org-install)
-;;
-;; Standard key bindings
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c b") 'org-iswitchb)
-(global-set-key (kbd "C-c c") 'org-capture)
 
-(setq org-log-done t)
+(set-register ?o (cons 'file "~/Documents/organizer.org"))
+(use-package org
+  :ensure t
+  :mode ("\\.org\\'"            . org-mode)
+  :mode ("\\.org_archive\\'"    . org-mode)
+  :mode ("\\.txt\\'"            . org-mode)
+  :bind (("C-c l"       . org-store-link)
+         ("C-c b"       . org-iswitchb)
+         ("C-c C-r"     . (lambda ()
+                            (interactive)
+                            (find-file "~/Documents/organizer.org")))
+         )
+  :config
+  (setq org-agenda-files         '()
+        org-capture-templates   '()
+        org-refile-targets      '((nil :maxlevel . 9))
+        org-completion-use-ido  nil)
 
-(defconst my-org-agenda-files-dir "~/Drives/Sync/Agenda")
-(defvar   my-org-agenda-files     ())
+  (setq org-refile-targets
+        (append org-refile-targets
+                '((notes-refile-targets      :maxlevel . 9))))
 
-(defun load-my-agenda-files ()
+  ;;; https://blog.aaronbieber.com/2017/03/19/organizing-notes-with-refile.html
+  (setq org-refile-use-outline-path             'file
+        org-outline-path-complete-in-steps      nil
+        org-refile-allow-creating-parent-nodes  'confirm)
+  (turn-on-auto-fill)
+ )
+
+(use-package org-capture
+  :after org
+  :bind (("C-c c"       . org-capture)
+         ("C-c n"       . (lambda ()
+                            (interactive)
+                            (org-capture nil "n")))
+         )
+  :config
+  (setq org-capture-templates
+        (append org-capture-templates
+                '(
+                  ("n" "Notes Entry" entry
+                   (file+headline "~/Documents/organizer.org" "Note")
+                   "* %^{Title} %^G\n  %U\n  %^{Attendees}p\n  %?"
+                   :clock-in t
+                   )
+                  )))
+  )
+  
+(defun notes-refile-targets ()
   "Load all agenda files identified in var: my-org-agenda-files-dir"
   (interactive)
-  (let* ((my-agenda-dir (expand-file-name my-org-agenda-files-dir))
-	 (my-top-dir    (expand-file-name (concat my-agenda-dir "/.."))))
-    (dolist (file (shell-find my-top-dir))
-      (unless (string-match-p "/Agenda/\\|/Bugs/" file)
-	(if (string-match-p ".*\\.org$" file)
-	    (make-symbolic-link file (concat my-agenda-dir "/"
-					     (basename file)) t))))
-    (setq my-org-agenda-files (directory-files my-agenda-dir t "^.*\\.org")))
-  (setq org-agenda-files my-org-agenda-files))
-
-;;; (load-my-agenda-files)
-
-(defconst my-org-Bugs-dir	"~/Documents/Bugs")
-(defun show-my-bugs ()
-  "Show status on all active bugs of interest"
-
-  (interactive)
-  (save-excursion
-    (save-restriction
-      (let ((org-agenda-files (directory-files (expand-file-name
-						my-org-Bugs-dir)
-					       t "^.*\\.org$")))
-	(org-agenda)
-	)))
-  (setq org-agenda-files my-org-agenda-files)
-  )
-
-;;;  (setq org-agenda-files '("~/Document/NOTES/2012-10"))
-
-(setq org-notes-dir (expand-file-name "~/Documents"))
-(defun org-notes ()
-  "Switch to my work dir"
-  (interactive)
-  (find-file org-notes-dir))
-
+  (let ((notes-files '(dummy)))
+    (dolist (file (shell-find "~/Documents/NOTES"))
+      (if (string-match-p ".*\\.org$" file)
+          (nconc notes-files (list file))))
+    (cdr notes-files)))
+                                
 (setq org-link-abbrev-alist
       '(("ddts"	. "https://cdetsng.cisco.com/webui/#view=")
 	("prrq" . "http://wwwin-tools.cisco.com/prrq/viewReview.do?queueName=ng-admin-dev&bugId=")
@@ -65,16 +71,12 @@
         ("cpol" . "https://cpol.cisco.com/cpol/patent.cgi?task=PatDetails&nav=y&patent_p=")
 	))
 
-(defun org-insert-cpol (num title)
-  "Insert a CPOL URL"
-  (interactive "nCPOL #? \nMTitle for CPOL# %s? ")
-  (insert (format "[[CPOL: %s][CPOL# %s: %s]]" num num title))
+(use-package org-crypt
+  :config
+  (org-crypt-use-before-save-magic)
+  (setq org-tags-exclude-from-inheritance '("crypt"))
+  (setq org-crypt-key "kannan@ieee.org")
   )
-
-(require 'org-crypt)
-(org-crypt-use-before-save-magic)
-(setq org-tags-exclude-from-inheritance (quote ("crypt")))
-(setq org-crypt-key "kannan@ieee.org")
 
 ;
 ;;; Local Variables:
